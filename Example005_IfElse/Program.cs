f014e5b774f13041160d95c7050a39270591fb7e
Timur <khasanov06031986@gmail.com>
2023-06-10 20:54:09 +0500

    Example005_ifElse project

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("ВВедите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "timur"){
+    Console.Write(" Hello, how are you Timur?");
+}
+else {
+    Console.Write(" Hello! " + username + " sorry, I'm didn't recognize you" );
+
+}
